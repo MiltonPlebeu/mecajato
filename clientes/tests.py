e4e30ed14c7882de68c8e77cd8c7cb1e1c1776d1
MiltonPlebeu <mplebeu@gmail.com>
2023-09-01 19:07:39 -0300

    Castro de Clientes

---
+++ clientes/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+b = input('Digite seu nome: ')
+a = input(f'{b} Digite seu RG: ')
+print(f'Obrigado {b}, seu RG é o: {a}. Desejamos um ótimo dia!!!')
+
+
